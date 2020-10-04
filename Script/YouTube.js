2c678c3925c597ba5c8c532bae426a560ed8c7aa
张博 <bo.zhang@beantechs.com>
2020-10-04 12:19:55 +0800

    update

--- Script/YouTube.js
+++ Script/YouTube.js
@@ -1,9 +1,15 @@
-#!name=YouTube
-#!desc=YouTube 去广告
-
-
+/*
 [Script]
 http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) script-path=https://raw.githubusercontent.com/deplives/Surge/master/Script/YouTube.js
 
 [MITM]
-hostname = %APPEND% *.googlevideo.com 
+hostname = *.googlevideo.com
+*/
+
+var data = {
+    body: "{}",
+    headers: {
+        "Content-Type": "multipart/byteranges"
+    }
+};
+$done({ response: data });
